e62264bf542d3fc09f38f681efa6c0bd7b1407fe
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-05-28 14:43:38 +0300

    Add files via upload

--- cli.sh
+++ cli.sh
@@ -4,7 +4,7 @@ nmcli con modify ens18 ipv4.method manual ipv4.addresses 3.3.3.2/30
 nmcli con modify ens18 ipv4.gateway 3.3.3.1
 
 nmcli con modify Проводное\ подключение\ 1 ipv4.method manual ipv4.addresses 4.4.4.2/30
-nmcli con modify Проводное\ подключение\ 1 ipv4.gateway 4.4.4.1 ipv4.dns 172.16.100.2 ipv4.dns-search hq.work + ipv4.routes "172.16.100.2/26 4.4.4.1"
+nmcli con modify Проводное\ подключение\ 1 ipv4.gateway 4.4.4.1 ipv4.dns 172.16.100.2 ipv4.dns-search hq.work + ipv4.routes 172.16.100.2/26 4.4.4.1
 
 systemctl restart NetworkManager
 
